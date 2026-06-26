dcc8efd056322650ec254ebb6a51d546a55bf670
SaTsSo <stsai30160@ufide.ac.cr>
2026-06-25 20:05:24 -0600

    Se crean las clases ProductoBase, Carrito y ControlArchivo

--- Cobro_Caja_Minisuper/src/main/java/GUI/Inicio.java
+++ Cobro_Caja_Minisuper/src/main/java/GUI/Inicio.java
@@ -1,4 +1,20 @@
 package GUI;
 
-public class Inicio {
+import javax.swing.*;
+
+public class Inicio extends JFrame {
+    private JPanel PanelInicio;
+
+
+    //Constructor
+    public Inicio() {
+        setContentPane(PanelInicio);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setEnabled(true);
+        setTitle("Notas");
+        setSize(1000, 1000);
+        setVisible(true);
+
+
+    }
 }
