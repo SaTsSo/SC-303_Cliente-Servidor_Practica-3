f3286a21e14a30edcca24cae3bdf978e77189ced
SaTsSo <stsai30160@ufide.ac.cr>
2026-06-25 21:37:14 -0600

    Se agregan los atributos y metodos para leer archivos txt en la clase ControlArchivo

--- Cobro_Caja_Minisuper/src/main/java/ControlArchivo.java
+++ Cobro_Caja_Minisuper/src/main/java/ControlArchivo.java
@@ -1,2 +1,67 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class ControlArchivo {
+
+    private String ruta = "productos.txt";
+
+    public static List<Producto> leerProductos() {
+
+        List<Producto> listaProductos = new ArrayList<>();
+
+        try {
+            DataInputStream lectura = new DataInputStream(new FileInputStream(ruta));
+
+            try {
+                while (true) {
+                    int codigo = lectura.readInt();
+                    String nombre = lectura.readUTF();
+                    double precio = lectura.readDouble();
+                    int stock = lectura.readInt();
+
+                    Producto producto = new Producto(codigo, nombre, precio, stock);
+
+                    listaProductos.add(producto);
+                }
+
+            } catch (EOFException e) {
+                cerrarArchivoLectura(lectura);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado: " + e.getMessage());
+
+        } catch (IOException e) {
+            System.out.println("Error de lectura: " + e.getMessage());
+        }
+
+        return listaProductos;
+    }
+
+
+    public static Producto buscarProducto(int codigoBuscado) {
+
+        List<Producto> productos = leerProductos();
+
+        for (Producto p : productos) {
+            if (p.getCodigo() == codigoBuscado) {
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+
+
+    public static void cerrarArchivoLectura(DataInputStream archivo) {
+        try {
+            archivo.close();
+        } catch (IOException e) {
+            System.out.println("Error al cerrar archivo: " + e.getMessage());
+        }
+    }
 }
+
