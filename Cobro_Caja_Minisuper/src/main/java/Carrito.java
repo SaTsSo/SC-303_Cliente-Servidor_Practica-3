52a9d67e4c07127f5378713c9a281a65a620eee9
SaTsSo <stsai30160@ufide.ac.cr>
2026-06-25 21:36:23 -0600

    Se agregan los atributos, constructores, getters, setters y metodos base para la clase Carrito

--- Cobro_Caja_Minisuper/src/main/java/Carrito.java
+++ Cobro_Caja_Minisuper/src/main/java/Carrito.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class Carrito {
+
+    //Atributos
+    private List<Producto> productos;
+
+    //Constructor
+    public Carrito(List<Producto> productos) {
+        productos = new ArrayList<>();
+    }
+
+    //Getter/Setters
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    //Metodos
+    public void agregarProducto(Producto producto, int cantidad) {
+        validarCantidad(cantidad);
+
+        for (int i = 0; i < cantidad; i++) {
+            productos.add(producto);
+        }
+    }
+
+    public void validarCantidad(int cantidad) {
+        if (cantidad < 0) {
+            throw new IllegalArgumentException("La cantidad no puede ser negativa");
+        }
+    }
+
 }
