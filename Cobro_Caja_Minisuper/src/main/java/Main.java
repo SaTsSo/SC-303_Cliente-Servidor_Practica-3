670b3512ca903eb3b21d133cb05ff29b7ff0f82d
SaTsSo <stsai30160@ufide.ac.cr>
2026-06-25 19:56:57 -0600

    Se genera proyecto base

---
+++ Cobro_Caja_Minisuper/src/main/java/Main.java
@@ -0,0 +1,13 @@
+/*
+GRUPO5
+VILLAFUERTE SOTELO YABETH DAVID
+VELASQUEZ RAMIREZ EBER JESUS
+TSAI SOLIS SAMUEL
+BENAVIDES HIDALGO BYRON ALEXIS
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
